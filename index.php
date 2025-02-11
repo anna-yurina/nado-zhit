1e3c94c18e6a064d7c4169a31e5129072f74e869
anna-yurina <jacka99@list.ru>
2025-02-11 20:07:20 +0300

    Изменила титул, слоган и Ñподвал

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Новости банка");
+$APPLICATION->SetTitle("Надо жить!");
 ?>
 <h1>СПАСИБО ПАПА!!!</h1>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
