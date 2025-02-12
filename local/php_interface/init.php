c05986e292dad387e3f1212b07409755bdc83b00
Виталий Юрин <v.yurin@qsoft.ru>
2025-02-12 19:00:35 +0300

    Обновление композера

---
+++ local/php_interface/init.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
+    require_once(dirname(__DIR__) . '/vendor/autoload.php');
+}
