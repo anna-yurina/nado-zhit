1f57b84c6ff1940b38103d8bb13ddea647408c8c
anna-yurina <jacka99@list.ru>
2025-02-15 11:51:24 +0300

    Создание подраздела Надо жить

--- romans/need_live/index.php
+++ romans/need_live/index.php
@@ -3,7 +3,6 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 global $APPLICATION;
 
-$APPLICATION->SetTitle("Романы");
+$APPLICATION->SetTitle("Цикл романов Надо жить");
 ?>
-
 <?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
