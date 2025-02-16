24be55a98db992ab166896f52be9f80047c08479
anna-yurina <jacka99@list.ru>
2025-02-16 22:20:05 +0300

    Создание стиля Создание стиля шапки страниц

--- local/templates/need_live/header.php
+++ local/templates/need_live/header.php
@@ -4,6 +4,8 @@ IncludeTemplateLangFile(__FILE__);
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <? if ($APPLICATION->GetCurPage(false) === '/'): ?>
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title><?$APPLICATION->ShowTitle()?></title>
@@ -12,24 +14,6 @@ IncludeTemplateLangFile(__FILE__);
 	<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH?>/common.css" />
 
 	<?$APPLICATION->ShowHead();?>
-
-	<!--[if lte IE 6]>
-	<style type="text/css">
-
-		#support-question {
-			background-image: none;
-			filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='./images/question.png', sizingMethod = 'crop');
-		}
-
-		#support-question { left: -9px;}
-
-		#banner-overlay {
-			background-image: none;
-			filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='./images/overlay.png', sizingMethod = 'crop');
-		}
-
-	</style>
-	<![endif]-->
 
 	<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH?>/colors.css" />
 
@@ -40,8 +24,8 @@ IncludeTemplateLangFile(__FILE__);
             <div id="header">
                 <div class="d-flex justify-content-beetween">
                     <div>
-                        <div class="custom-logo-div">
-                            <a href="<?= SITE_DIR ?>" class="custom-logo">
+                        <div class="custom-logo-div-main">
+                            <a href="<?= SITE_DIR ?>" class="custom-logo-main">
                                 Надо жить!
                             </a>
                         </div>
@@ -81,11 +65,55 @@ IncludeTemplateLangFile(__FILE__);
 
 			<div id="content-wrapper">
 				<div id="content">
-                    <div id="breadcrumb">
+					<div id="workarea-wrapper">
+                                <?$APPLICATION->ShowTitle(false);?>
+<?else:?>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<title><?$APPLICATION->ShowTitle()?></title>
+	<link rel="shortcut icon" type="image/x-icon" href="<?=SITE_TEMPLATE_PATH?>/favicon.ico" />
+
+	<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH?>/common.css" />
+
+	<?$APPLICATION->ShowHead();?>
+
+	<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH?>/colors.css" />
+
+</head>
+<body>
+		<div id="page-wrapper">
+
+			<div id="panel"><?$APPLICATION->ShowPanel();?></div>
+
+			<div id="header" class="header-inner">
+                    <a href="<?= SITE_DIR ?>" class="custom-logo-inner">
+                        Надо жить!
+                    </a>
+            </div>
+
+<?$APPLICATION->IncludeComponent("bitrix:menu", "top", array(
+	"ROOT_MENU_TYPE" => "top",
+	"MENU_CACHE_TYPE" => "Y",
+	"MENU_CACHE_TIME" => "36000000",
+	"MENU_CACHE_USE_GROUPS" => "Y",
+	"MENU_CACHE_GET_VARS" => array(
+	),
+	"MAX_LEVEL" => "1",
+	"CHILD_MENU_TYPE" => "left",
+	"USE_EXT" => "N",
+	"ALLOW_MULTI_SELECT" => "N"
+	),
+	false
+);?>
+
+
+			<div id="content-wrapper">
+				<div id="content">
+                    <div id="breadcrumb" class="custom-bc">
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
 						<?$APPLICATION->IncludeComponent("bitrix:breadcrumb", "custom_breadcrumb", array(
-                            "START_FROM" => "1",
+                            "START_FROM" => "0",
                             "PATH" => "",
                             "SITE_ID" => SITE_ID
                             ),
@@ -95,21 +123,9 @@ IncludeTemplateLangFile(__FILE__);
                         </nav>
 					</div>
 					<div id="workarea-wrapper">
-						<div id="left-menu">
-						<?$APPLICATION->IncludeComponent("bitrix:menu", "tree", array(
-							"ROOT_MENU_TYPE" => "leftfirst",
-							"MENU_CACHE_TYPE" => "Y",
-							"MENU_CACHE_TIME" => "36000000",
-							"MENU_CACHE_USE_GROUPS" => "Y",
-							"MENU_CACHE_GET_VARS" => array(
-							),
-							"MAX_LEVEL" => "4",
-							"CHILD_MENU_TYPE" => "left",
-							"USE_EXT" => "N",
-							"ALLOW_MULTI_SELECT" => "N"
-							),
-							false
-						);?>
-						</div>
+						<div id="workarea">
+							<div id="workarea-inner">
+							<h5>
                                 <?$APPLICATION->ShowTitle(false);?>
-
+                                </h5>
+<?endif;?>
