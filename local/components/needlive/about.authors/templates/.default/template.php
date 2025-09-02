4f087b9e907571b6f7f88b65fb8421e4afa28bfa
Виталий Юрин <v.yurin@qsoft.ru>
2025-09-02 19:15:24 +0300

    refactoring the about authors component

--- local/components/needlive/about.authors/templates/.default/template.php
+++ local/components/needlive/about.authors/templates/.default/template.php
@@ -1,34 +1,37 @@
-<div class="author-section">
-    <h3 class="author-name">Анна Морозова</h3>
-
-    <div class="author-row">
-        <div class="author-photo-wrap">
-            <img src="/upload/author_masha.png" alt="Анна Морозова" class="author-photo">
-        </div>
-        <div class="author-text">
-            <p>Пишу романы, живу на даче, люблю котов. Интроверт</p>
-        </div>
+<?php if (!empty($arResult['AUTHORS'])): ?>
+    <div class="author-section">
+        <h3 class="author-name"><?= GetMessage("AUTHOR_NAME") ?></h3>
+        <?php foreach($arResult["AUTHORS"] as $key => $values) {?>
+            <div class="author-row">
+                <?php if ($key % 2 !== 0) { ?>
+                    <div class="author-text">
+                        <p><?= $values['UF_BIOTEXT']?></p>
+                    </div>
+                    <div class="author-photo-wrap">
+                        <?php $img_path = CFile::GetPath($values['UF_BIOPHOTO']);?>
+                        <?= '<img src="'.$img_path.'" class="author-photo" alt="'.$img_path.'"/>';?>
+                    </div>
+                    <?php } else { ?>
+                    <div class="author-photo-wrap">
+                        <?php $img_path = CFile::GetPath($values['UF_BIOPHOTO']);?>
+                        <?= '<img src="'.$img_path.'" class="author-photo" alt="'.$img_path.'"/>';?>
+                    </div>
+                    <div class="author-text">
+                        <p><?= $values['UF_BIOTEXT']?></p>
+                    </div>
+                <?php } ?>
+            </div>
+        <?php } ?>
     </div>
+<?php endif;?>
 
-    <div class="author-row">
-        <div class="author-text">
-            <p>Соавтор, культуролог, осваиваю IT. Экстраверт</p>
-        </div>
-        <div class="author-photo-wrap">
-            <img src="/upload/author_anya.jpg" alt="Анна Морозова" class="author-photo">
-        </div>
+<?php if (!empty($arResult['LINKS'])): ?>
+    <div class="author-links">
+        <?php foreach($arResult["LINKS"] as $title => $values) {?>
+            <a href="<?= $values['UF_LINK']?>" target="_blank">
+                <?php $img_path = CFile::GetPath($values['UF_ICON']);?>
+                <?= '<img src="'.$img_path.'" width="30" alt="'.$img_path.'"/>';?>
+            </a>
+        <?php } ?>
     </div>
-</div>
-
-    <?php
-    if (!empty($arResult['ITEMS'])): ?>
-<div class="author-links">
-    <?php foreach($arResult["ITEMS"] as $title => $values) {?>
-        <a href="<?= $values['UF_LINK']?>" target="_blank">
-            <?php $img_path = CFile::GetPath($values['UF_ICON']);?>
-            <?= '<img src="'.$img_path.'" width="30" alt="'.$img_path.'"/>';?>
-            <?php } ?>
-        </a>
-
-</div>
 <?php endif;?>
